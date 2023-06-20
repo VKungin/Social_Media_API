1435210f9f65d81df8a4074f3b02772cc751d370
Vyacheslav Kungin <acheros4@gmail.com>
2023-06-20 16:42:03 +0300

    add filtering by hashtags

--- wall/views.py
+++ wall/views.py
@@ -13,6 +13,21 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.prefetch_related("hashtags")
     serializer_class = PostSerializer
 
+    @staticmethod
+    def _params_to_ints(qs):
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        hashtags = self.request.query_params.get("hashtags")
+
+        if hashtags:
+            hashtags_ids = self._params_to_ints(hashtags)
+            queryset = queryset.filter(hashtags__id__in=hashtags_ids)
+
+        return queryset.distinct()
+
 
 class HashtagViewSet(viewsets.ModelViewSet):
     queryset = Hashtag.objects.all()
